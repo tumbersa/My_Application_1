38fc76138357afd0c3d46453c1b94fa4f6c2b737
Галина Иванова <gleb.kapustin.98@mail.ru>
2023-03-26 22:57:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/my_application_1/App.kt
@@ -0,0 +1,8 @@
+package com.example.my_application_1
+
+import android.app.Application
+class App:Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/example/my_application_1/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.my_application_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {//редко используется
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {//редко используется
+        super.onStop()
+    }
+}
